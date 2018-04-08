c2884801eaeb83d4cbb277058a3071f0e2ce9933
Adam Wieckert <wiec1369@gmail.com>
2018-04-08 14:16:58 -0500

    XHR request coming back with syntax errors on parse.

--- main.js
+++ main.js
@@ -9,36 +9,54 @@ const printToDom = (stringToPrint, divID) => {
 
 const buttonEventListeners = () => {
     let buttonToClick = document.getElementById('challenge-button');
+    console.log("Button Selected: ", buttonToClick);
     buttonToClick.addEventListener('click', player1XHR);
 }
 
 const comparePlayers = (player1JSON, player2JSON) => {
     let stringToPrint = "";
+    let secondStringToPrint = "";
+    let thirdStringToPrint = "";
     stringToPrint += `<div class="player1-image col-md-4 col-md-offset-1">`;
-    stringToPrint += `<img src="./img/dead-pool1.jpg" alt="">`;
-    stringToPrint += `<h2>6000</h2>`;
+    stringToPrint += `<img src="${player1JSON.gravatar_url}" alt="">`;
+    stringToPrint += `<h2>${player2JSON.points.total}</h2>`;
     stringToPrint += `</div>`;
     stringToPrint += `<div class="player2-image col-md-4 col-md-offset-1">`;
-    stringToPrint += `<img src="./img/dead-pool2.jpg" alt="">`;
-    stringToPrint += `<h2>5000</h2>`;
+    stringToPrint += `<img src="${player2JSON.gravatar_url}" alt="">`;
+    stringToPrint += `<h2>${player2JSON.points.total}</h2>`;
     stringToPrint += `</div>`;
     printToDom(stringToPrint, "challenger-image-container");
     if(player1JSON.points.total > player2JSON.points.total){
-        let secondStringToPrint = "";
         secondStringToPrint += `<div class="col-md-12">`;
-        secondStringToPrint += `<h1>Deadpool Wins!!!!</h1>`;
+        secondStringToPrint += `<h1>${player1JSON.name} Wins!!!!</h1>`;
         secondStringToPrint += `</div>`;
         secondStringToPrint += `<div class="row">`;
         secondStringToPrint += `<div id="winner-badges" class="col-md-12">`;
         player1JSON.badges.forEach((badge, index) => {
-            secondStringToPrint += `<div id="badge${index}" class="player-badge">`;
-            secondStringToPrint += `<img src="${badge.icon_url}">`
-            secondStringToPrint += `</div>`;
+            thirdStringToPrint += `<div id="badge${index}" class="player-badge">`;
+            thirdStringToPrint += `<img src="${badge.icon_url}">`
+            thirdStringToPrint += `</div>`;
         });
         secondStringToPrint += `</div>`;
         secondStringToPrint += `</div>`;
+        printToDom(secondStringToPrint, "winner");
+        printToDom(thirdStringToPrint, "winner-badges");
     } else {
-
+        let secondStringToPrint = "";
+        secondStringToPrint += `<div class="col-md-12">`;
+        secondStringToPrint += `<h1>${player2JSON.name} Wins!!!!</h1>`;
+        secondStringToPrint += `</div>`;
+        secondStringToPrint += `<div class="row">`;
+        secondStringToPrint += `<div id="winner-badges" class="col-md-12">`;
+        player2JSON.badges.forEach((badge, index) => {
+            thirdStringToPrint += `<div id="badge${index}" class="player-badge">`;
+            thirdStringToPrint += `<img src="${badge.icon_url}">`
+            thirdStringToPrint += `</div>`;
+        });
+        secondStringToPrint += `</div>`;
+        secondStringToPrint += `</div>`;
+        printToDom(secondStringToPrint, "winner");
+        printToDom(thirdStringToPrint, "winner-badges");
     }
 }
 
@@ -48,6 +66,7 @@ const WTF = () => {
 
 const player2XHR = (player1JSON) => {
     const player2name = document.getElementById('player2').value;
+    console.log("Player 2 input: ", player2name);
     const player2URL = `https://teamtreehouse.com/${player2name}.json`;
     let myRequest = new XMLHttpRequest;
     myRequest.addEventListener('load', multiplePlayerStuff);
@@ -66,15 +85,18 @@ const getPlayer1JSON = () => {
     player2XHR(player1JSON);
 }
 
-const player1XHR = (functionToRun) => {
+const player1XHR = () => {
     const player1name = document.getElementById('player1').value;
+    console.log("Player 1 input: ", player1name);
     const player1URL = `https://teamtreehouse.com/${player1name}.json`;
+    console.log("player1URL: ", player1URL);
     let myRequest = new XMLHttpRequest;
     myRequest.addEventListener("load", getPlayer1JSON);
     myRequest.addEventListener("error", WTF);
     myRequest.open("GET", player1URL);
     myRequest.send();
 }
+
 
 const startApplication = () => {
     buttonEventListeners();
